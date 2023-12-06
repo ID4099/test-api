959954d85179c62826d9b6143fcaae5246eda94f
Alejandro Mardach <aamardach@gmail.com>
2023-12-06 11:53:08 -0300

    first commit from development

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
+const { log } = require('console');
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  log('hello world from Index');
   res.send('Hello World; welcome to AWS Ec2!')
 });
 
